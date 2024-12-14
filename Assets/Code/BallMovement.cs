608abb7d3c8ea3baf177630e7137461df88ab9cd
Ankur Banerjee <rukna2003@gmail.com>
2024-12-15 00:18:13 +0530

    Changed variable name rigidbody to rigid_body for naming conflicts warning

--- Assets/Code/BallMovement.cs
+++ Assets/Code/BallMovement.cs
@@ -1,22 +1,22 @@
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour {
-    Rigidbody2D rigidbody;
+    Rigidbody2D rigid_body;
     float jump_speed;
 
     void Start() {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigid_body = GetComponent<Rigidbody2D>();
         jump_speed = 200f;
     }
     
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            rigidbody.AddForce(Vector2.up * jump_speed);            
+            rigid_body.AddForce(Vector2.up * jump_speed);            
             int direction = Random.Range(0, 2);
             if(direction == 0) { 
-                rigidbody.AddForce(Vector2.left * 10f);
+                rigid_body.AddForce(Vector2.left * 10f);
             } else if(direction == 1) {
-                rigidbody.AddForce(Vector2.right * 10f);
+                rigid_body.AddForce(Vector2.right * 10f);
             }
         }
     }
