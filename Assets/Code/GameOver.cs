f637c076d9f9b65d2d8a4bbbcd5b70b6a1a39edc
Ankur Banerjee <rukna2003@gmail.com>
2024-12-23 03:23:54 +0530

    Fixed bug where ball flies to infinity and score can be spammed

--- Assets/Code/GameOver.cs
+++ Assets/Code/GameOver.cs
@@ -6,8 +6,11 @@ public class GameOver : MonoBehaviour {
 
     public AudioClip game_over_sfx;
     public GameObject high_score;
+    GameObject ball;
 
     private void OnCollisionEnter2D(Collision2D collision) {
+        ball = GameObject.Find("Ball");
+        ball.GetComponent<SpriteRenderer>().enabled = false;
         AudioSource.PlayClipAtPoint(game_over_sfx, transform.position);
         PlayerPrefs.SetInt("HighScoreCount", high_score.GetComponent<HighScore>().high_score_count);
         PlayerPrefs.Save();
