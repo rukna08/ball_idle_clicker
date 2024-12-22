2347a27268d4d6f444905b390d654d202b3e4e01
Ankur Banerjee <rukna2003@gmail.com>
2024-12-23 04:32:26 +0530

    Menu UI Changes According to State

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour {
     
-    public GameManager gameManager;
+    public GameManager game_manager;
     
     bool is_menu_open = false;
+    
+    GameObject menu_gfx;
+    GameObject paused_gfx;
+    GameObject unpaused_gfx;
+
+    void Start() {
+        menu_gfx = GameObject.Find("MenuGFX");
+        paused_gfx = menu_gfx.transform.GetChild(0).gameObject;
+        unpaused_gfx = menu_gfx.transform.GetChild(1).gameObject;
+    }
 
     public void PauseAndUnpause() {
         if(is_menu_open) {
-            gameManager.UnpauseGame();
+            game_manager.UnpauseGame();
+            paused_gfx.SetActive(true);
+            unpaused_gfx.SetActive(false);
         } else if(!is_menu_open) {
-           gameManager.PauseGame();
+            game_manager.PauseGame();
+            paused_gfx.SetActive(false);
+            unpaused_gfx.SetActive(true);
         }
         is_menu_open = !is_menu_open;
     }
